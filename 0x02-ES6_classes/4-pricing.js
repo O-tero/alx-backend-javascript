e71ea53c32804be1915bcffd49a93932d4638fa7
O-tero <oteronickbrian@gmail.com>
2022-12-22 22:15:47 +0300

    0x02-ES6_classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,29 +1,39 @@
+
+import Currency from './3-currency';
+
 export default class Pricing {
   constructor(amount, currency) {
-    (this.amount = amount), (this.currency = currency);
+    // Create objs
+    this._amount = amount;
+    this._currency = currency;
   }
 
-  get amount() {
-    return this._amount;
-  }
-
-  set amount(value) {
-    this._amount = value;
-  }
-
-  get currency() {
-    return this._currency;
-  }
-
-  set currency(value) {
-    this._currency = value;
-  }
+  // Methods
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
   }
 
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
+  }
+
+  // Setters
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
+    this._currency = newCurrency;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
   }
 }
