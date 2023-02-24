1d145433a15f694ee2392ec8622c87462336c3b6
O-tero <oteronickbrian@gmail.com>
2023-02-24 23:17:36 +0300

    making changes

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,12 @@
-/* eslint-disable */
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Andrew Tonui <https://github.com/Cyborg1999>
+ */
+class AppController {
+  static getHomepage (request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
